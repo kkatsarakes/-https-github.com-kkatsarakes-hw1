c65e68af93d59ffd01f266115068a784b8ebba92
Kiki Katsarakes <kiki.katsarakes@kellogg.northwestern.edu>
2025-04-21 22:52:10 +0000

    draft 1

--- kmdb.sql
+++ kmdb.sql
@@ -111,10 +111,16 @@
 .headers off
 
 -- Drop existing tables, so you'll start fresh each time this script is run.
--- TODO!
+DROP TABLE IF EXISTS
 
 -- Create new tables, according to your domain model
--- TODO!
+CREATE TABLE movies (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  title TEXT,
+  year_released INTEGER,
+  NPAA_rating TEXT,
+  studio TEXT
+);
 
 -- Insert data into your database that reflects the sample data shown above
 -- Use hard-coded foreign key IDs when necessary
